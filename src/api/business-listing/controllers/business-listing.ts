c7d21f94a5a3aee63cba1283c83c86683b7f7b31
priyankastro <priyankastro7@gmail.com>
2024-10-21 02:55:57 +0530

    business listing user email initimation

--- src/api/business-listing/controllers/business-listing.ts
+++ src/api/business-listing/controllers/business-listing.ts
@@ -8,14 +8,24 @@ export default factories.createCoreController('api::business-listing.business-li
     async update(ctx) {
         try {
             const response = await super.update(ctx);
+            const authorData = await strapi.entityService.findOne('api::business-listing.business-listing', response.data.id, {
+                populate: 'author',
+            });
+            console.log(authorData)
             if (process.env.APP_ENV === "Production") {
                 if (response.data.attributes && response.data.attributes.step_number === 5) {
                     const adminUsers = await strapi.db.query('admin::user').findMany();
-                    let emailToAddressList = adminUsers.map(x => x.email).join(',');
+                    let emailToAddressListAdmin = adminUsers.map(x => x.email).join(',');
                     await strapi.plugins['email'].services.email.send({
-                        to: emailToAddressList,
+                        to: emailToAddressListAdmin,
                         subject: 'New Business Listing Published - Chandapura.com',
-                        html: `A new Business Listing - <b>${response.data.attributes.name}</b> is posted and awaiting approval. Please review the item in below link and approve \n ${process.env.PUBLIC_URL}/admin/content-manager/collection-types/api::business-listing.business-listing/${response.data.id}`,
+                        html: `<p>A new Business Listing - <b>${response.data.attributes.name}</b> is posted and awaiting approval. Please review the item in below link for approval</p> ${process.env.PUBLIC_URL}/admin/content-manager/collection-types/api::business-listing.business-listing/${response.data.id}`,
+                    })
+                    let emailToAddressUser = authorData.author.email;
+                    await strapi.plugins['email'].services.email.send({
+                        to: emailToAddressUser,
+                        subject: 'New Business Listing Uploaded - Chandapura.com',
+                        html: `<p>A new Business Listing - <b>${response.data.attributes.name}</b> has been successfully uploaded. Team is currently reviwing the uploaded content, once approved, the listing will be published live and you will be intimated about the same via email.</p>`,
                     })
                 }
             }
