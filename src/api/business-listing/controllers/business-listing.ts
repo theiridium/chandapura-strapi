60e1dc69cd72651cf98e08b0896b28b7272ef0cb
priyankastro <priyankastro7@gmail.com>
2024-10-26 13:17:08 +0530

    Lifecycle update for business-listing

--- src/api/business-listing/controllers/business-listing.ts
+++ src/api/business-listing/controllers/business-listing.ts
@@ -11,7 +11,6 @@ export default factories.createCoreController('api::business-listing.business-li
             const authorData = await strapi.entityService.findOne('api::business-listing.business-listing', response.data.id, {
                 populate: 'author',
             });
-            console.log(authorData)
             if (process.env.APP_ENV === "Production") {
                 if (response.data.attributes && response.data.attributes.step_number === 5) {
                     const adminUsers = await strapi.db.query('admin::user').findMany();
@@ -20,13 +19,13 @@ export default factories.createCoreController('api::business-listing.business-li
                         to: emailToAddressListAdmin,
                         subject: 'New Business Listing Published - Chandapura.com',
                         html: `<p>A new Business Listing - <b>${response.data.attributes.name}</b> is posted and awaiting approval. Please review the item in below link for approval</p> ${process.env.PUBLIC_URL}/admin/content-manager/collection-types/api::business-listing.business-listing/${response.data.id}`,
-                    })
+                    });
                     let emailToAddressUser = authorData.author.email;
                     await strapi.plugins['email'].services.email.send({
                         to: emailToAddressUser,
                         subject: 'New Business Listing Uploaded - Chandapura.com',
                         html: `<p>A new Business Listing - <b>${response.data.attributes.name}</b> has been successfully uploaded. Team is currently reviwing the uploaded content, once approved, the listing will be published live and you will be intimated about the same via email.</p>`,
-                    })
+                    });
                 }
             }
             return response;
