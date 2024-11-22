7f665e7514a7f01fbc34823502ef1545aebc447f
priyankastro <priyankastro7@gmail.com>
2024-11-23 01:17:00 +0530

    update

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -34,6 +34,19 @@ export interface RealEstateRealEstate extends Schema.Component {
   };
 }
 
+export interface ContactContactDetails extends Schema.Component {
+  collectionName: 'components_contact_contact_details';
+  info: {
+    displayName: 'Contact Details';
+    description: '';
+  };
+  attributes: {
+    contact_name: Attribute.String;
+    contact_number: Attribute.String;
+    contact_email_id: Attribute.Email;
+  };
+}
+
 export interface PaymentPayment extends Schema.Component {
   collectionName: 'components_payment_payments';
   info: {
@@ -51,25 +64,12 @@ export interface PaymentPayment extends Schema.Component {
   };
 }
 
-export interface ContactContactDetails extends Schema.Component {
-  collectionName: 'components_contact_contact_details';
-  info: {
-    displayName: 'Contact Details';
-    description: '';
-  };
-  attributes: {
-    contact_name: Attribute.String;
-    contact_number: Attribute.String;
-    contact_email_id: Attribute.Email;
-  };
-}
-
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
       'real-estate.real-estate': RealEstateRealEstate;
-      'payment.payment': PaymentPayment;
       'contact.contact-details': ContactContactDetails;
+      'payment.payment': PaymentPayment;
     }
   }
 }
