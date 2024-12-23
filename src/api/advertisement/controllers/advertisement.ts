7c32806e19a2ce206126c86c422e7a8fa14064e2
priyankastro <priyankastro7@gmail.com>
2024-12-23 20:22:43 +0530

    ad test

--- src/api/advertisement/controllers/advertisement.ts
+++ src/api/advertisement/controllers/advertisement.ts
@@ -11,42 +11,39 @@ export default factories.createCoreController('api::advertisement.advertisement'
             const itemData = await strapi.entityService.findOne('api::advertisement.advertisement', response.data.id, {
                 populate: ['author', 'payment_details', 'payment_history'],
             });
-            console.log(itemData.author.email)
             if (process.env.APP_ENV === "Production") {
                 const adminUsers = await strapi.db.query('admin::user').findMany();
                 let emailToAddressListAdmin = adminUsers.map(x => x.email).join(',');
                 if (response.data.attributes && response.data.attributes.step_number === 4) {
-                    const yo = await strapi.plugins['email'].services.email.send({
+                    await strapi.plugins['email'].services.email.send({
                         to: emailToAddressListAdmin,
                         subject: 'New Advertisement Published - Chandapura.com',
-                        // html: `<p>A new Advertisement - <b>${response.data.attributes.name}</b> is posted and awaiting approval. Please review the item in below link for approval</p> ${process.env.PUBLIC_URL}/admin/content-manager/collection-types/api::business-listing.business-listing/${response.data.id}`,
-                        html: `<p>A new Advertisement</p>`,
+                        html: `<p>A new Advertisement - <b>${response.data.attributes.name}</b> is posted and awaiting approval. Please review the item in below link for approval</p> ${process.env.PUBLIC_URL}/admin/content-manager/collection-types/api::business-listing.business-listing/${response.data.id}`,
                     });
-                    console.log(yo)
-                    // let emailToAddressUser = itemData.author.email;
-                    // await strapi.plugins['email'].services.email.send({
-                    //     to: emailToAddressUser,
-                    //     subject: 'New Advertisement Uploaded - Chandapura.com',
-                    //     html: `<p>A new Advertisement - <b>${response.data.attributes.name}</b> has been successfully uploaded. Team is currently reviwing the uploaded content, once approved, the listing will be published live and you will be intimated about the same via email.</p>`,
-                    // });
+                    let emailToAddressUser = itemData.author.email;
+                    await strapi.plugins['email'].services.email.send({
+                        to: emailToAddressUser,
+                        subject: 'New Advertisement Uploaded - Chandapura.com',
+                        html: `<p>A new Advertisement - <b>${response.data.attributes.name}</b> has been successfully uploaded. Team is currently reviwing the uploaded content, once approved, the listing will be published live and you will be intimated about the same via email.</p>`,
+                    });
                 }
-                // if (response.data.attributes &&
-                //     response.data.attributes.step_number === 2 &&
-                //     itemData.payment_history.length > 0 &&
-                //     itemData.payment_details.expiry_date > new Date().toISOString()) {
-                //     await strapi.plugins['email'].services.email.send({
-                //         to: emailToAddressListAdmin,
-                //         subject: 'Images Updated for Advertisement - Chandapura.com',
-                //         html: `<p>Images Updated for Advertisement - <b>${response.data.attributes.name}</b> and awaiting approval. Please review the item in below link for approval</p>
-                //         <a href='${process.env.PUBLIC_URL}/admin/content-manager/collection-types/api::advertisement.advertisement/${response.data.id}'>Link to admin portal</a> `,
-                //     });
-                //     let emailToAddressUser = itemData.author.email;
-                //     await strapi.plugins['email'].services.email.send({
-                //         to: emailToAddressUser,
-                //         subject: 'Images Updated for Advertisement - Chandapura.com',
-                //         html: `<p>Images Updated for Advertisement - <b>${response.data.attributes.name}</b>. Team is currently reviwing the uploaded content, once approved, the listing will be published live and you will be intimated about the same via email.</p>`,
-                //     });
-                // }
+                if (response.data.attributes &&
+                    response.data.attributes.step_number === 2 &&
+                    itemData.payment_history.length > 0 &&
+                    itemData.payment_details.expiry_date > new Date().toISOString()) {
+                    await strapi.plugins['email'].services.email.send({
+                        to: emailToAddressListAdmin,
+                        subject: 'Images Updated for Advertisement - Chandapura.com',
+                        html: `<p>Images Updated for Advertisement - <b>${response.data.attributes.name}</b> and awaiting approval. Please review the item in below link for approval</p>
+                        <a href='${process.env.PUBLIC_URL}/admin/content-manager/collection-types/api::advertisement.advertisement/${response.data.id}'>Link to admin portal</a> `,
+                    });
+                    let emailToAddressUser = itemData.author.email;
+                    await strapi.plugins['email'].services.email.send({
+                        to: emailToAddressUser,
+                        subject: 'Images Updated for Advertisement - Chandapura.com',
+                        html: `<p>Images Updated for Advertisement - <b>${response.data.attributes.name}</b>. Team is currently reviwing the uploaded content, once approved, the listing will be published live and you will be intimated about the same via email.</p>`,
+                    });
+                }
             }
             return response;
 
